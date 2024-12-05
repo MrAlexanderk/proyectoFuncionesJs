703f25ed2e1867c83acd7081776e4ea4b2530fba
Tom <70593510+MrAlexanderk@users.noreply.github.com>
2024-12-05 20:12:00 -0300

    Let added

--- assets/js/2_arrow.js
+++ assets/js/2_arrow.js
@@ -1,3 +1,3 @@
-suma = (a, b) => a + b
+let suma = (a, b) => a + b
 
 // Me queda la duda en este ejercicio y el anterior si es necesario declarar un let o const antes de la función. Si pudieras responderme en el comentario sería bacán <3
